8e2f6b280537f335db44526671bd8cd837077fd4
EmiAr5 <earduzzo@gmail.com>
2023-05-15 14:53:58 -0300

    Actualización de crossorigin con el deploy de angular

--- src/main/java/com/portfolio/EmAr/controller/IdiomaController.java
+++ src/main/java/com/portfolio/EmAr/controller/IdiomaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/idioma")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliowebemar.web.app")
 public class IdiomaController {
 
     @Autowired
--- src/main/java/com/portfolio/EmAr/controller/ProyectoController.java
+++ src/main/java/com/portfolio/EmAr/controller/ProyectoController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliowebemar.web.app")
 public class ProyectoController {
 
     @Autowired
